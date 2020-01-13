75f340b287cfb6186de89022a011237e5880382f
zadieblack <37587898+zadieblack@users.noreply.github.com>
2020-01-13 18:26:36 -0500

    debugging

--- reddit.py
+++ reddit.py
@@ -5,26 +5,32 @@ import praw
 
 SUBREDDIT_NAME = 'erotica_ebooks'
 
-def PostToReddit(sLinkTitle, sLinkURL, iFlairID):
+def PostToReddit(sLinkTitle, sLinkURL, sFlairID):
     try:
         reddit = praw.Reddit('bot1')
 
         subreddit = reddit.subreddit(SUBREDDIT_NAME)
 
+        print("subreddit.title: " + sLinkTitle)
+        print("subreddit.url: " + sLinkURL)
+        print("subreddit.flair_id: " + sFlairID)
+        print("subreddit.nsfw: " + str(True))
+        print("subreddit.resubmit: " + str(False))
+
         subreddit.submit(title = sLinkTitle, 
                             url = sLinkURL, 
-                            flair_id = iFlairID, 
+                            flair_id = sFlairID, 
                             nsfw = True,
                             resubmit = False)
     except praw.exceptions.PRAWException as e:
         print("* ! PRAW Error: " + e.reason + " ! *")
 
 def PostToReddit_eebot(sLinkTitle, sLinkURL):
-    iFlairID = "0968a470-362f-11ea-a7cd-0e19e0d6966d"
+    sFlairID = "0968a470-362f-11ea-a7cd-0e19e0d6966d"
 
-    PostToReddit(sLinkTitle, sLinkURL, iFlairID)
+    PostToReddit(sLinkTitle, sLinkURL, sFlairID)
 
 def PostToReddit_botlust(sLinkTitle, sLinkURL):
-    iFlairID = "12906c9a-362f-11ea-ad00-0e251d7edbcb"
+    sFlairID = "12906c9a-362f-11ea-ad00-0e251d7edbcb"
 
-    PostToReddit(sLinkTitle, sLinkURL, iFlairID)
+    PostToReddit(sLinkTitle, sLinkURL, sFlairID)
