e322b7c3acf08ea1d2e442d65d442d6c0f505a9c
zadieblack <37587898+zadieblack@users.noreply.github.com>
2020-01-21 00:19:20 -0500

    working on drawing text on the covers

--- title/texttoimg2.py
+++ title/texttoimg2.py
@@ -52,42 +52,45 @@ def WrapText(sText, font, max_line_width):
      return Lines
 
 # Guesstimate an initial font size based on the # of characters         
-def GuesstimateFontSize(sText):
+def GuesstimateFontSize(sText, iMaxPointSize = 0):
     iFontSize = 3
   
-    iTextLen = len(sText)
-    if iTextLen <= 45:
-        iFontSize = 75
-    elif iTextLen <= 60:
-        iFontSize = 68
-    elif iTextLen <= 75:     #(+  45)
-        iFontSize = 60
-    elif iTextLen <= 90:     #(+  70)
-        iFontSize = 55
-    elif iTextLen <= 110:     #(+  80)
-        iFontSize = 50
-    elif iTextLen <= 150:     #(+ 185)
-        iFontSize = 45
-    elif iTextLen <= 250:
-        iFontSize = 40
-    elif iTextLen <= 400:
-        iFontSize = 33
-    elif iTextLen <= 1000:
-        iFontSize = 29
-    else: 
-        iFontSize = 25
+    if iMaxPointSize == 0:
+        iTextLen = len(sText)
+        if iTextLen <= 45:
+            iFontSize = 75
+        elif iTextLen <= 60:
+            iFontSize = 68
+        elif iTextLen <= 75:     #(+  45)
+            iFontSize = 60
+        elif iTextLen <= 90:     #(+  70)
+            iFontSize = 55
+        elif iTextLen <= 110:     #(+  80)
+            iFontSize = 50
+        elif iTextLen <= 150:     #(+ 185)
+            iFontSize = 45
+        elif iTextLen <= 250:
+            iFontSize = 40
+        elif iTextLen <= 400:
+            iFontSize = 33
+        elif iTextLen <= 1000:
+            iFontSize = 29
+        else: 
+            iFontSize = 25
+    else:
+        iFontSize = iMaxPointSize 
 
     return iFontSize 
 
 class BlockOfText():
-    def __init__(self, sText, sFontName, BoundingBoxSize):
+    def __init__(self, sText, sFontName, iMaxPointSize, BoundingBoxSize):
         self.Text = sText 
         self.FontName = sFontName
         self.BoundingBoxWidth = BoundingBoxSize[0]
         self.BoundingBoxHeight = BoundingBoxSize[1] 
-        self.TotalLineHeight = 0
+        self.TotLineHeight = 0
         self.DecreaseSizeBy = 3
-        self.FontSize = GuesstimateFontSize(sText)
+        self.FontSize = GuesstimateFontSize(sText, iMaxPointSize)
         self.Font = ImageFont.truetype(PATH + self.FontName, size = self.FontSize, index = 0)
         
         # wrap the text based on the bounding text box's width
@@ -117,7 +120,7 @@ class BlockOfText():
 
             self.TotLineHeight = self.CalcTotLineHeight()
 
-def FormatText(sText, BoxWidth, BoxHeight, Color):
+def FormatText(sText, BoxWidth, BoxHeight, FontName, Color, iMaxPointSize):
     Lines = []
      
     # how much the text in the box is offset from the box
@@ -129,20 +132,21 @@ def FormatText(sText, BoxWidth, BoxHeight, Color):
     offset_height = round(BoxHeight - (BoxHeight * yOffset * 2), 0)
 
     TextBlock = BlockOfText(sText, 
-                            "Lapidary 333 Bold Italic.otf", 
+                            FontName, 
+                            iMaxPointSize,
                             (offset_width, offset_height))
                
     ImgTxt = Image.new('RGBA', (BoxWidth, BoxHeight))
     draw = ImageDraw.Draw(ImgTxt)
                
     y_text = 0
-    for x in range(0, len(TextBlock.Lines)):
+    for line in TextBlock.Lines:
         width, height = (0, 0)
-        if TextBlock.Lines[x].isspace() or TextBlock.Lines[x] == "":
+        if line.isspace() or line == "":
             width, height = TextBlock.Font.getsize("a")
             height = height / 2
         else:
-            width, height = TextBlock.Font.getsize(TextBlock.Lines[x])
+            width, height = TextBlock.Font.getsize(line)
             width = width - (width * .05) # for some reason width for this font is a little off, so we tack on a 5% fudge
 
             #draw.text(((offset_width - width)/2, (offset_height - TextBlock.TotalLineHeight)/2 + y_text), 
@@ -150,8 +154,10 @@ def FormatText(sText, BoxWidth, BoxHeight, Color):
             #          font = TextBlock.Font, 
             #          fill = Color)
 
-            draw.text(((offset_width - width)/2, 0), 
-                      TextBlock.Lines[x], 
+            draw.text(
+                      ((offset_width - width)/2, 
+                       (offset_height - TextBlock.TotLineHeight)/2 + y_text), 
+                      line, 
                       font = TextBlock.Font, 
                       fill = Color)
         y_text += height
@@ -190,9 +196,11 @@ def CreateImg(TitleTweet):
         color = 'rgba(0, 0, 0, 255)' # color should eventually come from template
         
         ImgAuthorNameTxt = FormatText(TitleTweet.AuthorName(),
-                                      BoxWidth = 831, #571, 
-                                      BoxHeight = 71, #70)
-                                      Color = color)
+                                      BoxWidth = 861,  
+                                      BoxHeight = 78, 
+                                      FontName = "Lapidary 333 Bold Italic.otf",
+                                      Color = color,
+                                      iMaxPointSize = 67)
         #ImgAuthorNameTxt = DrawText(ImgBase.size, 
         #                            .643,  #571 1600 - 571 = 1029  1029/1600 = .643 
         #                            .928,   #70  971 - 70 = 901     901/971 = .928                           
@@ -202,7 +210,7 @@ def CreateImg(TitleTweet):
         ## draw title
       
         # combine the text and base images
-        ImgBase.paste(ImgAuthorNameTxt, (70, 571), mask = ImgAuthorNameTxt)
+        ImgBase.paste(ImgAuthorNameTxt, (55, 540), mask = ImgAuthorNameTxt)
       
         # save the edited image
         RGBImgOut = ImgBase.convert('RGB')
